0707124a298ef62681d8d8f76644453156d7e38b
Meg Galvez <meg@oakmaine.com>
2019-05-14 17:56:41 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -41,5 +41,30 @@ class Song
   end 
   
   def self.alphabetical
+    self.all.sort_by do |song|
+      song.name
+    end 
   end 
-end
+  
+  def self.new_from_filename(filename) 
+  song_array = filename.split(" - ")
+    song_array[1] = song_array[1].chomp(".mp3")
+    song = self.new
+    song.name = song_array[1]
+    song.artist_name = song_array[0]
+    song 
+  end 
+
+
+  def self.create_from_filename(filename)
+    result = self.new_from_filename(filename)
+    song = self.create
+    song.name = result.name
+    song.artist_name = result.artist_name
+    song 
+  end 
+  
+  def self.destroy_all
+    @@all.clear
+  end 
+ end 
